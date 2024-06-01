389ea54db40f3f63b0e2ba8ffe62e330577c494b
Sushant Butta <sushant.butta@gmail.com>
2024-06-01 21:25:04 +0530

    added access modifier

--- src/main/java/org/buttas/Main.java
+++ src/main/java/org/buttas/Main.java
@@ -1,7 +1,37 @@
 package org.buttas;
 
+class Employee{
+
+    private int eid;
+    private String ename;
+
+    public int getEid() {
+        return eid;
+    }
+
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+
+
+
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee e = new Employee();
+        e.setEid(1);
+        e.setEname("Sushant");
+
+        System.out.println(e.getEid());
+        System.out.println(e.getEname());
     }
 }
